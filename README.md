e4a59247705c81f72732a5d1b8cac1f4990cece8
Torsten Curdt <tcurdt@vafer.org>
2016-02-04 14:10:57 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Small utils when I didn't want to pull in the whole `lodash`.
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+module.exports.throw = function(msg) {
+  throw new Error(msg)
+}
+
+module.exports.spread = function(fn) {
+  return function (values) {
+    return fn.apply(null, values)
+  }
+}
+
+module.exports.merge = function() {
+  const objects = Array.prototype.slice.call(arguments)
+  const r = {}
+  objects.forEach(function(object){
+    if (object) {
+      Object.keys(object).forEach(function(key){
+        r[key] = object[key]
+      })
+    }
+  })
+  return r
+}
+
+module.exports.max = function(values) {
+  let m = undefined
+  values.forEach(function(v) {
+    if (v != undefined) {
+      m = (m != undefined) ? Math.max(m, v) : v
+    }
+  })
+  return m
+}
+
+let _objid = 1
+module.exports.objectId = function(obj) {
+  if (obj == null) {
+    return null
+  }
+  if (obj._objid == null) {
+    obj._objid = _objid++
+  }
+  return obj._objid
+}
+
+module.exports.stripBasedir = function(basedir, s) {
+  if (basedir) {
+    // FIXME
+    return s.replace(new RegExp('^' + basedir + '/'), '')
+  } else {
+    return s
+  }
+}
